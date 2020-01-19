f2553c7bae061188f2dc889c8f7d9d4e6d3801bf
Petar Simic <simic.petar@outlook.com>
2020-01-19 16:08:47 +0100

    Admin panel and Login screen created

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+
+    namespace ContactList
+    {
+        public class AccountController : Controller
+        {
+            public IActionResult Login()
+            {
+                return View();
+            }
+        }
+    }
